c103093deebdabad31b6a0755849ded268e70468
sbrunocamara <101370202+sbrunocamara@users.noreply.github.com>
2023-09-18 22:10:03 -0300

    Ajustes allowed methods

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ header("Access-Control-Allow-Origin:*");
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Origin");
+header('Access-Control-Allow-Credentials: true ');
+header('Access-Control-Allow-Headers: *');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
