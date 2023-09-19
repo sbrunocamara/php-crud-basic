5d124e4d0593cf029b5b7298c455ef4420782f15
sbrunocamara <101370202+sbrunocamara@users.noreply.github.com>
2023-09-18 21:58:21 -0300

    Ajustes error display

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ use crudController\AuthController;
 use crudMiddleware\Authenticate;
 use crudRepositories\PersonRepository;
 
+ini_set('display_errors', 'Off');
 
 require_once __DIR__ . '\vendor\autoload.php';
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin:*");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
